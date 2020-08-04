46a3ef6c940236f139e9aea48c97ece9ecc375a4
Edwina Palmer <hekamara@gmail.com>
2020-08-04 15:58:53 -0400

    Answered questions for assignments

---
+++ py1/datatypes.py
@@ -0,0 +1,11 @@
+# Write a print statement that combines x + y into the integer value 12
+
+x = 5
+y = "7"
+
+print(x + int(y))
+
+
+# Write a print statement that combines x + y into the string value 57
+
+print(str(x) + y)
---
+++ py1/modules.py
@@ -0,0 +1,25 @@
+import sys
+# See docs for the sys module: https://docs.python.org/3.7/library/sys.html
+
+# Print out the command line arguments in sys.argv, one per line:
+for arg in sys.argv:
+    print(arg)
+
+# Print out the OS platform you're using:
+print(sys.platform)
+
+# Print out the version of Python you're using:
+print(sys.version)
+
+
+import os
+# See the docs for the OS module: https://docs.python.org/3.7/library/os.html
+
+# Print the current process ID
+print(os.getuid())
+
+# Print the current working directory (cwd):
+print(os.getcwd())
+
+# Print out your machine's login name
+print(os.getlogin())
